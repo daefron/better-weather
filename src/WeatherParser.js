93b5194f74830e9353275ee32605414b891100a0
Daefron <thomas_evans@outlook.com>
2025-04-29 11:16:17 +1000

    Removed locations in water

--- src/WeatherParser.js
+++ src/WeatherParser.js
@@ -1,25 +1,27 @@
 class Weather {
-    constructor(dailyData, index) {
-      this.date = dailyData.time[index];
-      this.tempMax = dailyData.temperature_2m_max[index];
-      this.tempMin = dailyData.temperature_2m_min[index];
-      this.rainChance = dailyData.precipitation_probability_max[index];
-      this.rainHours = dailyData.precipitation_hours[index];
-      this.windMax = dailyData.wind_speed_10m_max[index];
-    }
+  constructor(dailyData, index) {
+    this.date = dailyData.time[index];
+    this.tempMax = dailyData.temperature_2m_max[index];
+    this.tempMin = dailyData.temperature_2m_min[index];
+    this.rainChance = dailyData.precipitation_probability_max[index];
+    this.rainHours = dailyData.precipitation_hours[index];
+    this.windMax = dailyData.wind_speed_10m_max[index];
   }
+}
 
-  class Location {
-    constructor(data) {
-      this.suburb = data.suburb || "Unknown suburb";
-      this.longitude = data.longitude;
-      this.latitude = data.latitude;
-      this.dates = data.daily.time.map(
-        (_, index) => new Weather(data.daily, index)
-      );
-    }
+class Location {
+  constructor(data) {
+    this.suburb = data.suburb || "Unknown suburb";
+    this.longitude = data.longitude;
+    this.latitude = data.latitude;
+    this.dates = data.daily.time.map(
+      (_, index) => new Weather(data.daily, index)
+    );
   }
+}
 
-  export function parseData(weatherData) {
-    return weatherData.map((dataPoint) => new Location(dataPoint));
-  }
+export function parseData(weatherData) {
+  return weatherData
+    .filter((dataPoint) => dataPoint.suburb) //removes locations in water
+    .map((dataPoint) => new Location(dataPoint));
+}
