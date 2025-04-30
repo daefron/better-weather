f1d59d46e24ccf0cf87951f3486170da7847149b
Daefron <thomas_evans@outlook.com>
2025-04-30 16:25:18 +1000

    Moved stats to bottom

--- src/App.jsx
+++ src/App.jsx
@@ -128,38 +128,6 @@ function App() {
           >
             Better Weather
           </h1>
-          {changeLayout ? (
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                gap: 10,
-                boxSizing: "border-box",
-                paddingInline: 10,
-                fontSize: 16,
-              }}
-            >
-              <p style={{ flexGrow: 1 }}>
-                {new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(
-                  new Date(centerPoint.dates[Math.floor(activeHour / 24)].date)
-                )}{" "}
-                {new Intl.DateTimeFormat("en-AU", {
-                  day: "numeric",
-                  month: "numeric",
-                }).format(
-                  new Date(centerPoint.dates[Math.floor(activeHour / 24)].date)
-                )}{" "}
-                -{" "}
-                {activeHour % 12 !== 11
-                  ? ((activeHour + 1) % 12) + " " + AMPM
-                  : 12 + (AMPM === "AM" ? "PM" : "AM")}
-              </p>
-              <p>{centerPoint.hours[activeHour].temp}°C</p>
-              <p>{centerPoint.hours[activeHour].rainChance}% rain</p>
-              <p>{centerPoint.hours[activeHour].windMax}km/h</p>
-            </div>
-          ) : null}
         </header>
         <main
           style={{
@@ -194,6 +162,43 @@ function App() {
         >
           {changeLayout ? (
             <>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  gap: 10,
+                  boxSizing: "border-box",
+                  paddingInline: 10,
+                  paddingBlock:5,
+                  fontSize: 16,
+                }}
+              >
+                <p style={{ flexGrow: 1 }}>
+                  {new Intl.DateTimeFormat("en-US", {
+                    weekday: "short",
+                  }).format(
+                    new Date(
+                      centerPoint.dates[Math.floor(activeHour / 24)].date
+                    )
+                  )}{" "}
+                  {new Intl.DateTimeFormat("en-AU", {
+                    day: "numeric",
+                    month: "numeric",
+                  }).format(
+                    new Date(
+                      centerPoint.dates[Math.floor(activeHour / 24)].date
+                    )
+                  )}{" "}
+                  -{" "}
+                  {activeHour % 12 !== 11
+                    ? ((activeHour + 1) % 12) + " " + AMPM
+                    : 12 + (AMPM === "AM" ? "PM" : "AM")}
+                </p>
+                <p>{centerPoint.hours[activeHour].temp}°C</p>
+                <p>{centerPoint.hours[activeHour].rainChance}% rain</p>
+                <p>{centerPoint.hours[activeHour].windMax}km/h</p>
+              </div>
               <div
                 style={{
                   display: "flex",
