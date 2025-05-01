1b6d59a46720f3ffd4e5580369456f411e830580
Daefron <thomas_evans@outlook.com>
2025-05-01 11:48:15 +1000

    Moved settings to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { getStats } from "./DataProcessing";
 import Map from "./Components/Map";
 import NavBar from "./Components/NavBar/NavBar";
 import SearchBar from "./Components/SearchBar/SearchBar";
+import Settings from "./Components/Settings/Settings";
 
 function App() {
   //user inputs
@@ -202,38 +203,10 @@ function App() {
             inputRef={inputRef}
           />
           {changeLayout ? null : !loading ? (
-            <div
-              id="inputs"
-              style={{
-                width: "auto",
-                marginBlock: 10,
-                marginInline: 15,
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <form
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  gap: 10,
-                }}
-              >
-                <label htmlFor="searchRadius">
-                  Search radius: {`${radiusInput}km`}
-                </label>
-                <input
-                  type="range"
-                  id="searchRadius"
-                  style={{ flexGrow: 1 }}
-                  min="25"
-                  max="200"
-                  step="1"
-                  value={radiusInput}
-                  onChange={(e) => setRadiusInput(e.target.value)}
-                ></input>
-              </form>
-            </div>
+            <Settings
+              radiusInput={radiusInput}
+              setRadiusInput={setRadiusInput}
+            />
           ) : null}
         </footer>
       </div>
---
+++ src/Components/Settings/Settings.jsx
@@ -0,0 +1,36 @@
+export default function Settings({ radiusInput, setRadiusInput }) {
+  return (
+    <div
+      id="inputs"
+      style={{
+        width: "auto",
+        marginBlock: 10,
+        marginInline: 15,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <form
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: 10,
+        }}
+      >
+        <label htmlFor="searchRadius">
+          Search radius: {`${radiusInput}km`}
+        </label>
+        <input
+          type="range"
+          id="searchRadius"
+          style={{ flexGrow: 1 }}
+          min="25"
+          max="200"
+          step="1"
+          value={radiusInput}
+          onChange={(e) => setRadiusInput(e.target.value)}
+        ></input>
+      </form>
+    </div>
+  );
+}
