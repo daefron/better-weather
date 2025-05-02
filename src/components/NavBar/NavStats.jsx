413db57d12f7c1d2b680ba20d769161d20e14a3e
Daefron <thomas_evans@outlook.com>
2025-05-02 10:40:07 +1000

    Navstats refactor

--- src/components/NavBar/NavStats.jsx
+++ src/components/NavBar/NavStats.jsx
@@ -1,4 +1,24 @@
 export default function NavStats({ centerPoint, selectedHour, amPm }) {
+  const dateIndex = Math.floor(selectedHour / 24);
+  const date = new Date(centerPoint.dates[dateIndex].date);
+  const weekdayContent = new Intl.DateTimeFormat("en-AU", {
+    weekday: "short",
+  }).format(date);
+  const dateContent = new Intl.DateTimeFormat("en-AU", {
+    day: "numeric",
+    month: "numeric",
+  }).format(date);
+  const timeContent =
+    selectedHour % 12 !== 11
+      ? ((selectedHour + 1) % 12) + " " + amPm
+      : 12 + (amPm === "AM" ? "PM" : "AM");
+  const dateTimeContent =
+    weekdayContent + " " + dateContent + " - " + timeContent;
+
+  const hourCache = centerPoint.hours[selectedHour];
+  const tempContent = hourCache.temp + "°C";
+  const rainContent = hourCache.rainChance + "% rain";
+  const windContent = hourCache.windMax + "km/h";
   return (
     <div
       style={{
@@ -6,33 +26,17 @@ export default function NavStats({ centerPoint, selectedHour, amPm }) {
         display: "flex",
         justifyContent: "space-between",
         gap: 10,
-        border:"outset 1px black",
+        border: "outset 1px black",
         boxSizing: "border-box",
         paddingInline: 10,
         paddingBlock: 5,
         fontSize: 16,
       }}
     >
-      <p style={{ flexGrow: 1 }}>
-        {new Intl.DateTimeFormat("en-US", {
-          weekday: "short",
-        }).format(
-          new Date(centerPoint.dates[Math.floor(selectedHour / 24)].date)
-        )}{" "}
-        {new Intl.DateTimeFormat("en-AU", {
-          day: "numeric",
-          month: "numeric",
-        }).format(
-          new Date(centerPoint.dates[Math.floor(selectedHour / 24)].date)
-        )}{" "}
-        -{" "}
-        {selectedHour % 12 !== 11
-          ? ((selectedHour + 1) % 12) + " " + amPm
-          : 12 + (amPm === "AM" ? "PM" : "AM")}
-      </p>
-      <p>{centerPoint.hours[selectedHour].temp}°C</p>
-      <p>{centerPoint.hours[selectedHour].rainChance}% rain</p>
-      <p>{centerPoint.hours[selectedHour].windMax}km/h</p>
+      <p style={{ flexGrow: 1 }}>{dateTimeContent}</p>
+      <p>{tempContent}</p>
+      <p>{rainContent}</p>
+      <p>{windContent}</p>
     </div>
   );
 }
