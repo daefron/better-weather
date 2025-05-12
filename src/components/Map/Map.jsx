b0ade6e9f3be5166647f641339e05da74806c5a6
Daefron <thomas_evans@outlook.com>
2025-05-12 12:48:16 +1000

    Moved TempMarker out of GoogleMap component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -17,20 +17,140 @@ if (!googleApiKey) {
   throw new Error("Google Maps Api key is missing.");
 }
 
-function GoogleMap() {
+function TempMarker({ data, i }) {
   const {
-    mapData,
-    userPoint,
     selectedHour,
-    normalizedRadius,
-    ringCount,
     unitType,
     selectedLocation,
     setSelectedLocation,
     useHours,
     clickListMap,
+    setViewArea,
+    userPoint,
+  } = useWeatherState();
+
+  const style = {
+    display: "flex",
+    gap: 5,
+    borderRadius: 4,
+    padding: 3,
+    fontSize: 13,
+    color: "black",
+    border: "2px solid rgba(0,0,0,0.2)",
+  };
+  const contentValue = useHours
+    ? data.hours[selectedHour][unitType]
+    : data.dates[Math.floor(selectedHour / 24)][unitType];
+
+  const colorRatio = useHours
+    ? contentValue / userPoint.hours[selectedHour][unitType]
+    : contentValue / userPoint.dates[Math.floor(selectedHour / 24)][unitType];
+
+  let content, positiveValue, negativeValue;
+  switch (unitType) {
+    case "temp":
+      if (selectedLocation === i) {
+        content = `${data.suburb} - ${contentValue}°`;
+      } else {
+        content = `${contentValue}°`;
+      }
+      if (colorRatio < 1) {
+        negativeValue = 255;
+        positiveValue = 255 * colorRatio;
+      } else {
+        positiveValue = 255;
+        negativeValue = 255 * (2 - colorRatio) * 0.8;
+      }
+      break;
+    case "rainChance":
+      if (selectedLocation === i) {
+        content = `${data.suburb} - ${contentValue}%`;
+      } else {
+        content = `${contentValue}%`;
+      }
+
+      //sets to same color if same value
+      if (colorRatio === Infinity || isNaN(colorRatio)) {
+        negativeValue = 255;
+        positiveValue = 255;
+        break;
+      }
+      if (colorRatio < 1) {
+        negativeValue = 255 * colorRatio;
+        positiveValue = 255;
+      } else {
+        positiveValue = 255 * (2 - colorRatio);
+        negativeValue = 255;
+      }
+      break;
+    case "windMax":
+      if (selectedLocation === i) {
+        content = `${data.suburb} - ${contentValue}km/h`;
+      } else {
+        content = `${contentValue}km/h`;
+      }
+      if (colorRatio > 1) {
+        negativeValue = 255;
+        positiveValue = 255 * colorRatio;
+      } else {
+        positiveValue = 255;
+        negativeValue = 255 * (2 - colorRatio) * 0.8;
+      }
+      break;
+  }
+  style.backgroundColor = `RGBA(${negativeValue}, ${positiveValue}, 0, 1)`;
+  let zIndex = 1;
+  if (selectedLocation === i) {
+    style.textDecoration = "underline";
+    style.fontSize = 16;
+    zIndex = 20;
+  } else {
+    style.textDecoration = "";
+    zIndex = 1;
+  }
+  return (
+    <AdvancedMarker
+      style={style}
+      position={{ lat: data.latitude, lng: data.longitude }}
+      zIndex={zIndex}
+      onClick={() => {
+        setViewArea(
+          selectedLocation === i
+            ? { lat: userPoint.lat, lng: userPoint.lng }
+            : { lat: data.latitude, lng: data.longitude }
+        );
+        setSelectedLocation(selectedLocation === i ? null : i);
+      }}
+      collisionBehavior={CollisionBehavior.OPTIONAL_AND_HIDES_LOWER_PRIORITY}
+    >
+      <p style={{ color: "black" }}>{content}</p>
+      {selectedLocation === i ? (
+        <FontAwesomeIcon
+          icon={faList}
+          color="black"
+          style={{
+            height: 18,
+            width: 18,
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            clickListMap(i);
+          }}
+        />
+      ) : null}
+    </AdvancedMarker>
+  );
+}
+
+function GoogleMap() {
+  const {
+    userPoint,
+    normalizedRadius,
+    ringCount,
     viewArea,
     setViewArea,
+    setSelectedLocation,
+    mapData,
   } = useWeatherState();
 
   const map = useMap();
@@ -61,120 +181,6 @@ function GoogleMap() {
     south: userPoint.lat - boundDist * 0.7,
     west: userPoint.lng - boundDist * 0.7,
   };
-
-  function TempMarker({ data, i }) {
-    const style = {
-      display: "flex",
-      gap: 5,
-      borderRadius: 4,
-      padding: 3,
-      fontSize: 13,
-      color: "black",
-      border: "2px solid rgba(0,0,0,0.2)",
-    };
-    const contentValue = useHours
-      ? data.hours[selectedHour][unitType]
-      : data.dates[Math.floor(selectedHour / 24)][unitType];
-
-    const colorRatio = useHours
-      ? contentValue / userPoint.hours[selectedHour][unitType]
-      : contentValue / userPoint.dates[Math.floor(selectedHour / 24)][unitType];
-
-    let content, positiveValue, negativeValue;
-    switch (unitType) {
-      case "temp":
-        if (selectedLocation === i) {
-          content = `${data.suburb} - ${contentValue}°`;
-        } else {
-          content = `${contentValue}°`;
-        }
-        if (colorRatio < 1) {
-          negativeValue = 255;
-          positiveValue = 255 * colorRatio;
-        } else {
-          positiveValue = 255;
-          negativeValue = 255 * (2 - colorRatio) * 0.8;
-        }
-        break;
-      case "rainChance":
-        if (selectedLocation === i) {
-          content = `${data.suburb} - ${contentValue}%`;
-        } else {
-          content = `${contentValue}%`;
-        }
-
-        //sets to same color if same value
-        if (colorRatio === Infinity || isNaN(colorRatio)) {
-          negativeValue = 255;
-          positiveValue = 255;
-          break;
-        }
-        if (colorRatio < 1) {
-          negativeValue = 255 * colorRatio;
-          positiveValue = 255;
-        } else {
-          positiveValue = 255 * (2 - colorRatio);
-          negativeValue = 255;
-        }
-        break;
-      case "windMax":
-        if (selectedLocation === i) {
-          content = `${data.suburb} - ${contentValue}km/h`;
-        } else {
-          content = `${contentValue}km/h`;
-        }
-        if (colorRatio > 1) {
-          negativeValue = 255;
-          positiveValue = 255 * colorRatio;
-        } else {
-          positiveValue = 255;
-          negativeValue = 255 * (2 - colorRatio) * 0.8;
-        }
-        break;
-    }
-    style.backgroundColor = `RGBA(${negativeValue}, ${positiveValue}, 0, 1)`;
-    let zIndex = 1;
-    if (selectedLocation === i) {
-      style.textDecoration = "underline";
-      style.fontSize = 16;
-      zIndex = 20;
-    } else {
-      style.textDecoration = "";
-      zIndex = 1;
-    }
-    return (
-      <AdvancedMarker
-        style={style}
-        position={{ lat: data.latitude, lng: data.longitude }}
-        zIndex={zIndex}
-        onClick={() => {
-          setViewArea(
-            selectedLocation === i
-              ? { lat: userPoint.lat, lng: userPoint.lng }
-              : { lat: data.latitude, lng: data.longitude }
-          );
-          setSelectedLocation(selectedLocation === i ? null : i);
-        }}
-        collisionBehavior={CollisionBehavior.OPTIONAL_AND_HIDES_LOWER_PRIORITY}
-      >
-        <p style={{ color: "black" }}>{content}</p>
-        {selectedLocation === i ? (
-          <FontAwesomeIcon
-            icon={faList}
-            color="black"
-            style={{
-              height: 18,
-              width: 18,
-            }}
-            onClick={(e) => {
-              e.stopPropagation();
-              clickListMap(i);
-            }}
-          />
-        ) : null}
-      </AdvancedMarker>
-    );
-  }
   return (
     <Map
       mapId="mainMap"
